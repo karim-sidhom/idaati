3b6c5c1a4cb232cca19d07d9f014e006a23d5677
karim-sidhom <karimsidhom@proton.me>
2026-06-22 21:46:50 +0100

    new

---
+++ sw (2).js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'idhaati-v1';
+const urlsToCache = [
+  './',
+  './idhaati_RADIO_FIXED.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache).catch(err => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('./idhaati_RADIO_FIXED.html');
+      });
+    })
+  );
+});
